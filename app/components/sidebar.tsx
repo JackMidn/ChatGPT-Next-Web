679a381a23ffb36972fbd3f0018686a11143e10b
JackMidn <41774703+JackMidn@users.noreply.github.com>
2024-12-06 15:26:07 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           ChatGPT
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          田俊杰  18827633252
+               田俊杰  18827633252
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
