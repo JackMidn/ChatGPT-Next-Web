ab7a23556df3f51bc012c4f4927b14892a26b229
JackMidn <41774703+JackMidn@users.noreply.github.com>
2024-12-06 15:13:43 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,10 +10,10 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "chatGPT",
+  description: "田俊杰 18827633252",
   appleWebApp: {
-    title: "NextChat",
+    title: "chatGPT",
     statusBarStyle: "default",
   },
 };
